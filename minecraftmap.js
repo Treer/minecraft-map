18d3a3fa8c52f62fe77fb4a2507457cdd0fa04d5
Treer <treer.git@the-bordello.com>
2014-05-12 04:21:44 +1000

    Major refactoring to allow settings to be specified in src url. Still needs screen size to be passed in so labellevel can be calculated, and rendering needs to read from the new settings system

--- minecraftmap.js
+++ minecraftmap.js
@@ -1,15 +1,18 @@
-
 var cClickRadius = 12;   // How far from the center of the icon is clickable
 var cMapRange    = 6000; // measured in minecraft blocks from the center
 var cTextOffset  = 14;   // How far under the center of the icon should the text be drawn
 var cCustomIconIndexStart = 32; // IconIndexes with this value or higher should be loaded from gCustomIcons
+var gCustomIcons = new Image();
+var gCustomIconsLoaded = false;
+
+var gXOffset     = 0;    // Sets the x coord for the center of the map
+var gZOffset     = 0;    // Sets the z coord for the center of the map
+
+
+/*
 
 var gLocationInfo = parseURL(location);
 
-var gCustomIcons = new Image();
-var gCustomIconsLoaded = false;
-var gXOffset     = 0;    // Sets the x coord for the center of the map
-var gZOffset     = 0;    // Sets the z coord for the center of the map
 
 // Set any constants specified by the URL (instead of using the default value)
 if ('range' in gLocationInfo.params) {	
@@ -38,7 +41,7 @@ if ('z' in gLocationInfo.params) {
 	var new_z = parseInt(gLocationInfo.params.z);
 	if (!isNaN(new_z)) gZOffset = new_z
 }	
-
+*/
 
 // Code snippet
 function isEmpty(str) {
@@ -121,6 +124,92 @@ var LocationType = {
 
 // Object constructor functions
 // ----------------------------
+
+function MapConfiguration() {
+
+	// Leave fields undefined unless specifically set (or call SetDefaults())
+}
+
+MapConfiguration.prototype.SetDefaults = function() {
+
+	// MapDataUri has no default - it MUST be set from the "src" param on the URL.
+	if (!('LabelsLevel'    in this)) this.LabelsLevel = 0;
+	if (!('MapRange'       in this)) this.MapRange = cMapRange;
+	if (!('Title'          in this)) this.Title = 'Map of the Overworld';
+	if (!('Blurb'          in this)) this.Blurb = 'Use up/down or mousewheel to zoom, drag to scroll';
+	if (!('CustomIconsUri' in this)) this.CustomIconsUri = '';
+	if (!('X'              in this)) this.X = 0;
+	if (!('Z'              in this)) this.Z = 0;
+}
+
+MapConfiguration.prototype.AssignFrom = function(sourceConfig) {
+
+	if ('MapDataUri'     in sourceConfig) this.MapDataUri     = sourceConfig.MapDataUri;
+	if ('LabelsLevel'    in sourceConfig) this.LabelsLevel    = sourceConfig.LabelsLevel;
+	if ('MapRange'       in sourceConfig) this.MapRange       = sourceConfig.MapRange;
+	if ('Title'          in sourceConfig) this.Title          = sourceConfig.Title;
+	if ('Blurb'          in sourceConfig) this.Blurb          = sourceConfig.Blurb;
+	if ('CustomIconsUri' in sourceConfig) this.CustomIconsUri = sourceConfig.CustomIconsUri;
+	if ('X'              in sourceConfig) this.X              = sourceConfig.X;
+	if ('Z'              in sourceConfig) this.Z              = sourceConfig.Z;
+}
+
+MapConfiguration.prototype.GetXTranslationFunction = function(mapSize) {
+	var halfMapSize = mapSize / 2;
+	var mapX = this.X;
+	var mapRange = this.MapRange;
+	
+	return function(coord) {
+		// this closure syntax doesn't work, fix it
+		return ((coord - mapX) * halfMapSize / mapRange) + halfMapSize;
+	}
+}
+
+MapConfiguration.prototype.GetZTranslationFunction = function(mapSize) {
+	var halfMapSize = mapSize / 2;
+	var mapZ = this.Z;
+	var mapRange = this.MapRange;
+
+	return function(coord) {
+		// this closure syntax doesn't work, fix it
+		return ((coord - mapZ) * halfMapSize / mapRange) + halfMapSize;
+	}
+}
+
+MapConfiguration.prototype.AssignFromRow = function(rowString) {
+
+	var posEquals = rowString.indexOf('=');
+	if (posEquals > 0) {
+		var key = rowString.substring(0, posEquals).trim().toLowerCase();
+		var value = rowString.slice(posEquals + 1).trim();
+		
+		if (key == 'z') {
+			var new_z = parseInt(value);
+			if (!isNaN(new_z)) this.Z = new_z;
+		}
+		if (key == 'x') {
+			var new_x = parseInt(value);
+			if (!isNaN(new_x)) this.X = new_x;
+		}
+		if (key == 'labelslevel') {
+			var new_LabelsLevel = parseInt(value);
+			if (!isNaN(new_LabelsLevel)) this.LabelsLevel = new_LabelsLevel;
+		}
+		if (key == 'maprange') {
+			var new_MapRange = parseInt(value);
+			if (!isNaN(new_MapRange)) this.MapRange = new_MapRange;
+		}
+		if (key == 'title' && isString(value)) {
+			this.Title = unquoteString(value);
+		}
+		if (key == 'blurb' && isString(value)) {
+			this.Blurb = unquoteString(value);
+		}
+		if (key == 'icons' && isString(value)) {
+			this.CustomIconsUri = unquoteString(value);
+		}
+	}
+}
 
 // x, z: coords in Minecraft.
 // Type: should be an element of LocationType
@@ -210,19 +299,25 @@ function unquoteString(str) {
  
 function parseTextLocations(data, callback) {
 
+	var config = new MapConfiguration();
 	var locationList = [];
 
 	lines = data.split('\n');
 	var i = 0;
 	for(i = 0; i < lines.length; i++) {
 		var newLocation = createLocationFromRow(lines[i]);	
-		if (newLocation instanceof Location) locationList.push(newLocation);		
+		if (newLocation instanceof Location) {
+			locationList.push(newLocation);		
+		} else {
+			config.AssignFromRow(lines[i]);
+		}
 	}
-	callback(locationList);
+	callback(config, locationList);
 }
 
 function parseHtmlLocations(data, callback) {
 	
+	var config = new MapConfiguration();
 	var locationList = [];
 	
 	var htmlDom = jQuery.parseHTML( data );
@@ -240,7 +335,11 @@ function parseHtmlLocations(data, callback) {
 			);
 			
 			var newLocation = createLocationFromRow(rowString);	
-			if (newLocation instanceof Location) locationList.push(newLocation);					
+			if (newLocation instanceof Location) {
+				locationList.push(newLocation);					
+			} else {
+				config.AssignFromRow(rowString);
+			}
 		}
 	);
 
@@ -250,15 +349,155 @@ function parseHtmlLocations(data, callback) {
 			$(this).find('li').each(
 				function() {			
 					var newLocation = createLocationFromRow(this.textContent);	
-					if (newLocation instanceof Location) locationList.push(newLocation);					
+					if (newLocation instanceof Location) {
+						locationList.push(newLocation);
+					} else {
+						config.AssignFromRow(this.textContent);
+					}
 				}
 			);			
 		}
 	);
 	
-	callback(locationList);
+	callback(config, locationList);
 }
  
+// callback will be given two arguments - a dictionary of settings and an array of Location instances
+function getSettingsAndMapLocations(callback) {
+
+	var configFromUrl = getConfigurationFromUrl();
+	
+	if ('MapDataUri' in configFromUrl) {
+		getMapDataAndLocationsFromUrl(
+			configFromUrl.MapDataUri,  
+			function(configFromAjax, locationsFromAjax) {
+			
+				var mapConfig = new MapConfiguration();
+				mapConfig.SetDefaults();
+				mapConfig.AssignFrom(configFromUrl);
+				mapConfig.AssignFrom(configFromAjax);
+				
+				ApplyMapConfiguration(mapConfig);
+				
+				if (!isEmpty(mapConfig.CustomIconsUri)) {
+					// Load the custom icons
+					
+					// I'm getting the impression there is no reliable way to wait for
+					// an image to load, see caveats in http://api.jquery.com/load-event/		
+					// If that's the case then custom icons won't work on browsers with broken
+					// onload event.
+					$(gCustomIcons).bind({
+						load: function() {
+							gCustomIconsLoaded = true;		
+							callback(mapConfig, locationsFromAjax);
+						},
+						error: function() {
+							// Image didn't load, probably a 404
+							callback(mapConfig, locationsFromAjax);
+						}
+					});		
+					gCustomIcons.src = gLocationInfo.params.icons;
+					
+				} else {	
+					callback(mapConfig, locationsFromAjax);
+				}
+			}
+		);
+	} else {
+		alert('no "src=" url was specified to scrape the location data from.');
+	}	
+	
+	function ApplyMapConfiguration(config) {
+
+		gXOffset = config.X;
+		gZOffset = config.Z;
+				
+		document.title = config.Title;
+		$("#mainTitle").text(config.Title);
+		$("#tagline").text(config.blurb);
+	}	
+
+		
+	function getMapDataAndLocationsFromUrl(dataUrl, callback) {
+		if (isString(dataUrl)) {	
+			var dataTypeIsText = dataUrl.match(/\.txt$|\.csv$/); // Assume HTML unless the dataUrl ends in .txt or .csv
+					
+			$.ajax({
+				 url: dataUrl,
+				 dataType: (dataTypeIsText ? 'text' : 'html'),
+				 success: function(data, textStatus, jqXHR) {
+					if (dataTypeIsText) {
+						parseTextLocations(data, callback);
+					} else {
+						parseHtmlLocations(data, callback);
+					}
+				 },
+				 error:function(jqXHR, textStatus, errorThrown){
+					alert('Failed to load locations from src "' + dataUrl + '", something went wrong: ' + textStatus + ', ' + errorThrown);
+				}
+			});
+					
+		} else {
+			alert('Internal error: dataUrl not string');
+		}	
+	}
+	
+
+	function getConfigurationFromUrl() {
+	
+		var result = new MapConfiguration();
+
+		var locationInfo = parseURL(location);
+		
+		// if "labelslevel" is specified on the URL, then only display labels when
+		// the map is zoomed in more levels than the value of labelslevel.
+		// i.e. 0 means always show labels, while 2 means don't show labels unless zoomed twice or more.
+		if ('labelslevel' in locationInfo.params) {
+			result.LabelsLevel = locationInfo.params.labelslevel;
+		}	
+
+		// Set any constants specified by the URL (instead of using the default value)
+		if ('range' in locationInfo.params) {	
+			result.MapRange = locationInfo.params.range;
+		}
+
+		// if "title" is specified on the URL then relabel the page
+		if ('title' in locationInfo.params  && isString(locationInfo.params.title)) {
+			$("#mainTitle").text(decodeURIComponent(locationInfo.params.title));
+			result.Title = locationInfo.params.title;
+		}	
+
+		// if "blurb" is specified on the URL then change the tag line
+		if ('blurb' in locationInfo.params  && isString(locationInfo.params.blurb)) {
+			result.Blurb = decodeURIComponent(locationInfo.params.blurb);
+		}	
+		
+		// if "icons" is specified on the URL then set the CustomIconsUri to load the images.
+		if ('icons' in locationInfo.params && isString(locationInfo.params.icons)) {
+			result.CustomIconsUri = locationInfo.params.icons;
+		}
+
+		// if "x" is specified on the URL then change the center of the map
+		if ('x' in locationInfo.params) {
+			var new_x = parseInt(locationInfo.params.x);
+			if (!isNaN(new_x)) result.X = new_x
+		}
+
+		// if "z" is specified on the URL then change the center of the map
+		if ('z' in locationInfo.params) {
+			var new_z = parseInt(locationInfo.params.z);
+			if (!isNaN(new_z)) result.Z = new_z
+		}	
+		
+		if ('src' in locationInfo.params && isString(locationInfo.params.src)) {		
+			result.MapDataUri = decodeURIComponent(locationInfo.params.src);
+		}
+		
+		return result;
+	}
+}
+ 
+ /*
 // callback will be given one argument - an array of Location instances
 function getMapLocations(callback) {
 	
@@ -311,9 +550,9 @@ function getMapLocations(callback) {
 		}	
 	}
 }
+*/
 
-
-function drawMapDetails(canvas, locations, iconsOnly)
+function drawMapDetails(canvas, config, locations, iconsOnly)
 {
 	var ctx = canvas.getContext("2d");
 	var mapSize = canvas.width > canvas.height ? canvas.width : canvas.height;
@@ -321,6 +560,9 @@ function drawMapDetails(canvas, locations, iconsOnly)
 
 	var tilesImage = document.getElementById('map-tileset');
 	
+	var translateCoord_x = config.GetXTranslationFunction(mapSize);
+	var translateCoord_z = config.GetZTranslationFunction(mapSize);
+	/*	
 	function translateCoord_x(coord)
 	{
 		return ((coord - gXOffset) * halfMapSize / cMapRange) + halfMapSize;
@@ -329,7 +571,7 @@ function drawMapDetails(canvas, locations, iconsOnly)
 	function translateCoord_z(coord)
 	{
 		return ((coord - gZOffset) * halfMapSize / cMapRange) + halfMapSize;
-	}
+	}*/
 	
 	function drawMultilineCenteredText(x, y, text) {
 
@@ -455,13 +697,13 @@ function drawMapDetails(canvas, locations, iconsOnly)
 	}
 }
 
-function createMapImageInDiv(divElementName, aWidth, aHeight, locations, iconsOnly) {
+function createMapImageInDiv(divElementName, aWidth, aHeight, config, locations, iconsOnly) {
 
 	var canvas = document.createElement('canvas');
 	canvas.width = aWidth;
 	canvas.height = aHeight;
 	
-	drawMapDetails(canvas, locations, iconsOnly);	
+	drawMapDetails(canvas, config, locations, iconsOnly);	
 	var areaMapId = CreateAreaMapInDiv(divElementName, aWidth, aHeight, locations);
 	
 	var newImage = document.createElement('img');
