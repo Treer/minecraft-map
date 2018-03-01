Treer/minecraft-map

commit 79d3358e92a9834538457e902f44313fea06c3fb

fix some typos and some typescript language changes (as I learn the language)

src/scripts/MinecraftMap.config.ts
@@ -341,7 +341,7 @@ namespace MinecraftMap {
 			// if "hideorigin" or "hidescale" is on the url then set ShowOrigin to false, likewise with ShowScale
 			if ('hideorigin' in locationInfo.params) this.ShowOrigin = false;
 			if ('hidescale' in locationInfo.params)  this.ShowScale  = false;
-			// or showoroigin and showscale could be specified explicitly
+			// or showorigin and showscale could be specified explicitly
 			if ('showorigin' in locationInfo.params && isString(locationInfo.params.showorigin)) {
 				this.ShowOrigin = stringToBool(locationInfo.params.showorigin);
 			}
@@ -392,7 +392,7 @@ namespace MinecraftMap {
 			if ('oceangooglesrc' in locationInfo.params && isString(locationInfo.params.oceangooglesrc)) {
 
 				if (locationInfo.params.oceangooglesrc.toLowerCase().indexOf('http') == 0) {
-					// User has used googlesrc when they should have used src. Rather than
+					// User has used oceangooglesrc when they should have used oceansrc. Rather than
 					// explain the error just correct it.
 					this.OceanMapUri = locationInfo.params.oceangooglesrc;
 				} else {
@@ -439,26 +439,18 @@ namespace MinecraftMap {
 
 	export class SuppressableLabel {
 
-		text:            string;
-		displayOverride: LabellingStyleOverride;
 		suppress:        boolean;
 		always:          boolean;
 
 		// Constructor
 		// text: the text of the label.
-		// labellingStyleOverride: an enumeration of type LabellingStyleOverride indicating whether the text should
+		// displayOverride: an enumeration of type LabellingStyleOverride indicating whether the text should
 		// be suppressed from the map rendering (only shown on hover etc.), always drawn regardless of the labellingStyle
 		// of the zoom level, or drawn when suitable (default)
-		constructor(text: string, labellingStyleOverride?: LabellingStyleOverride) {
-			this.text = text;
-
-			if (labellingStyleOverride === undefined) {
-				this.displayOverride = LabellingStyleOverride.normal;
-			} else {
-				this.displayOverride = labellingStyleOverride
-			}
-			this.suppress = this.displayOverride == LabellingStyleOverride.suppress;
-			this.always   = this.displayOverride == LabellingStyleOverride.always;
+        constructor(public text: string,
+                    public displayOverride = LabellingStyleOverride.normal) {
+			this.suppress = this.displayOverride === LabellingStyleOverride.suppress;
+			this.always   = this.displayOverride === LabellingStyleOverride.always;
 		}
 
 		toString(): string {
@@ -474,16 +466,14 @@ namespace MinecraftMap {
 
 			var result = new SuppressableLabel(markedupLabel);
 
-			if (isString(markedupLabel)) {
-				var trimLabelStr = trim(markedupLabel);
-				if (trimLabelStr.length >= 2) {
-					if (trimLabelStr[0] == cLabel_DontDrawChar && trimLabelStr[trimLabelStr.length - 1] == cLabel_DontDrawChar) {
-						result = new SuppressableLabel(trimLabelStr.substring(1, trimLabelStr.length - 1), LabellingStyleOverride.suppress);
-					} else if (trimLabelStr[0] == cLabel_AlwaysDrawChar && trimLabelStr[trimLabelStr.length - 1] == cLabel_AlwaysDrawChar) {
-						result = new SuppressableLabel(trimLabelStr.substring(1, trimLabelStr.length - 1), LabellingStyleOverride.always);
-					}
-				}
-			}
+            var trimLabelStr = trim(markedupLabel);
+            if (trimLabelStr.length >= 2) {
+                if (trimLabelStr[0] == cLabel_DontDrawChar && trimLabelStr[trimLabelStr.length - 1] == cLabel_DontDrawChar) {
+                    result = new SuppressableLabel(trimLabelStr.substring(1, trimLabelStr.length - 1), LabellingStyleOverride.suppress);
+                } else if (trimLabelStr[0] == cLabel_AlwaysDrawChar && trimLabelStr[trimLabelStr.length - 1] == cLabel_AlwaysDrawChar) {
+                    result = new SuppressableLabel(trimLabelStr.substring(1, trimLabelStr.length - 1), LabellingStyleOverride.always);
+                }
+            }
 			return result;
 		}
 	}

src/scripts/MinecraftMap.utils.ts
@@ -15,11 +15,11 @@ namespace MinecraftMap {
 	}
 
 	export function isString(str) {
-		return (typeof str == 'string' || str instanceof String);
+		return (typeof str === 'string' || str instanceof String);
 	}
 
 	export function isNotEmptyString(str) {
-		return (typeof str == 'string' || str instanceof String) && str.length > 0;
+		return (typeof str === 'string' || str instanceof String) && str.length > 0;
 	}
 
 	export function isFunction(item) {

commit 951cdfa84ea9db8d049a3aa351e8dd2ea2cbe4c2

Fix issue introduced in 79d3358e92a9834538457e902f44313fea06c3fb (map locations without labels would cause a crash, as the trim() function doesn't check for undefined)

src/scripts/MinecraftMap.config.ts
@@ -447,8 +447,8 @@ namespace MinecraftMap {
 		// displayOverride: an enumeration of type LabellingStyleOverride indicating whether the text should
 		// be suppressed from the map rendering (only shown on hover etc.), always drawn regardless of the labellingStyle
 		// of the zoom level, or drawn when suitable (default)
-        constructor(public text: string,
-                    public displayOverride = LabellingStyleOverride.normal) {
+		constructor(public text: string,
+		            public displayOverride = LabellingStyleOverride.normal) {
 			this.suppress = this.displayOverride === LabellingStyleOverride.suppress;
 			this.always   = this.displayOverride === LabellingStyleOverride.always;
 		}
@@ -466,14 +466,16 @@ namespace MinecraftMap {
 
 			var result = new SuppressableLabel(markedupLabel);
 
-            var trimLabelStr = trim(markedupLabel);
-            if (trimLabelStr.length >= 2) {
-                if (trimLabelStr[0] == cLabel_DontDrawChar && trimLabelStr[trimLabelStr.length - 1] == cLabel_DontDrawChar) {
-                    result = new SuppressableLabel(trimLabelStr.substring(1, trimLabelStr.length - 1), LabellingStyleOverride.suppress);
-                } else if (trimLabelStr[0] == cLabel_AlwaysDrawChar && trimLabelStr[trimLabelStr.length - 1] == cLabel_AlwaysDrawChar) {
-                    result = new SuppressableLabel(trimLabelStr.substring(1, trimLabelStr.length - 1), LabellingStyleOverride.always);
-                }
-            }
+			if (isString(markedupLabel)) { // Handle case where markedupLabel is undefined (this is still possible because project was ported to typescript rather than written in it from scratch with strong typing)
+				var trimLabelStr = trim(markedupLabel);
+				if (trimLabelStr.length >= 2) {
+					if (trimLabelStr[0] == cLabel_DontDrawChar && trimLabelStr[trimLabelStr.length - 1] == cLabel_DontDrawChar) {
+						result = new SuppressableLabel(trimLabelStr.substring(1, trimLabelStr.length - 1), LabellingStyleOverride.suppress);
+					} else if (trimLabelStr[0] == cLabel_AlwaysDrawChar && trimLabelStr[trimLabelStr.length - 1] == cLabel_AlwaysDrawChar) {
+						result = new SuppressableLabel(trimLabelStr.substring(1, trimLabelStr.length - 1), LabellingStyleOverride.always);
+					}
+				}
+			}
 			return result;
 		}
 	}